83f7a57866d404683be67fb2a7623833a999dd49
kamlesh2236-ks <adityaanmol223678@gmail.com>
2026-07-16 09:08:41 +0530

    fix: remove unnecessary blank line in App component

--- src/App.jsx
+++ src/App.jsx
@@ -119,7 +119,7 @@ export default function App() {
                       </SuperAdminRoute>
                     }
                   />
-
+                  
                   {/* Protected Routes */}
                   <Route
                     path="/dashboard"
